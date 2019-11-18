49f56b2f0b0dc899974b7d01accfade08374a5b3
997843911@qq.com <997843911@qq.com>
2019-11-18 20:02:35 +0800

    hello my redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+let appState = {
+    title: {
+        text: "React.js 小书",
+        color: "red"
+    },
+    content: {
+        text: "React.js 小书内容",
+        color: "blue"
+    }
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
+renderAPP(appState);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+function renderAPP(appState) {
+    renderTitle(appState.title);
+    renderContent(appState.content);
+}
+
+function renderTitle(title) {
+    let titleDOM = document.getElementById("title");
+    titleDOM.innerHTML = title.text;
+    titleDOM.style.color = title.color;
+}
+
+function renderContent(content) {
+    let contentDOM = document.getElementById("content");
+    contentDOM.innerHTML = content.text;
+    contentDOM.style.olor = content.color;
+}
+
+

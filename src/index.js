3b73b80dde3e82b80d8df585711ecc8650d23e61
997843911@qq.com <997843911@qq.com>
2019-11-19 12:08:23 +0800

    将stateChanger和appState合并到createStore中,stateChanger变成了纯函数

--- src/index.js
+++ src/index.js
@@ -1,41 +1,34 @@
-let appState = {
-  title: {
-    text: "React.js 小书",
-    color: "red"
-  },
-  content: {
-    text: "React.js 小书内容",
-    color: "blue"
-  }
-};
-function dispatch(action) {
-  switch (action.type) {
-    case "UPDATE_TITLE_TEXT":
-      appState.title.text = action.text;
-      break;
-    case "UPDATE_TITLE_COLOR":
-      appState.title.color = action.color;
-      break;
-    default:
-      break;
-  }
-}
-
-function createStore(state, stateChanger) {
+function createStore(reducer) {
   //抽取出来createStore
+  let state=null;
   let listeners = [];
-  let subscribe = listener => listeners.push(listener);
+  let subscribe = listener => listeners.push(listener); //订阅，这里可以用来订阅渲染函数
   let getState = () => state;
   let dispatch = action => {
-    state = stateChanger(state, action); //覆盖之前的state
+    state = reducer(state, action); //覆盖之前的state
     listeners.forEach(listener => {
-      listener();
+      listener();//执行订阅的函数
     });
   };
+  dispatch({});
   return { getState, dispatch, subscribe };
 }
 
 function stateChanger(state, action) {
+    if (!state) {
+        //初始化state
+        return {
+            title: {
+              text: "React.js 小书",
+              color: "red"
+            },
+            content: {
+              text: "React.js 小书内容",
+              color: "blue"
+            }
+          }
+    }
+
   switch (action.type) {
     case "UPDATE_TITLE_TEXT":
       return {
@@ -83,7 +76,7 @@ function renderContent(newContent, oldContent = {}) {
   contentDOM.style.color = newContent.color;
 }
 
-const store = createStore(appState, stateChanger);
+const store = createStore(stateChanger);
 let oldState = store.getState(); // 缓存旧的 state
 store.subscribe(() => {
   let newState = store.getState(); // 数据可能变化，获取新的 state
@@ -93,4 +86,4 @@ store.subscribe(() => {
 
 renderAPP(store.getState()); // 首次渲染页面
 store.dispatch({ type: "UPDATE_TITLE_TEXT", text: "《React.js 小书》" }); // 修改标题文本
-store.dispatch({ type: "UPDATE_TITLE_COLOR", color: "blue" }); // 修改标题颜色
+store.dispatch({ type: "UPDATE_TITLE_COLOR", color: "green" }); // 修改标题颜色
